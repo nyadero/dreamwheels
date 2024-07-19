6355ed3e5848cd15d280ff38a6b814a4c53f1c87
nyadero <briannyadero443@gmail.com>
2024-07-19 16:08:15 +0300

    deleted files

--- src/main/resources/database/data.sql
+++
@@ -1,6 +0,0 @@
-USING dreamwheels;
-
-USING vehicle_mileage::integer
-
-INSERT INTO products (brand, name, category, price, description, imageUrl)
-VALUES("Samsung", "Samsung galaxy s22+", "Electronics", 247000.00, "Clean samsung galaxy s22+ ultra for sale", "image_url");
