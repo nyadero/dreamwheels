8fe2757a6093fb01b1e0ce5fc3b7554506cd3f61
nyadero <briannyadero443@gmail.com>
2024-07-19 19:47:39 +0300

    add columns specific to vehicle

--- src/main/java/com/dreamwheels/dreamwheels/garage/entity/Vehicle.java
+++ src/main/java/com/dreamwheels/dreamwheels/garage/entity/Vehicle.java
@@ -1,6 +1,12 @@
 package com.dreamwheels.dreamwheels.garage.entity;
 
+import com.dreamwheels.dreamwheels.garage.enums.DriveTrain;
+import com.dreamwheels.dreamwheels.garage.enums.EngineAspiration;
+import com.dreamwheels.dreamwheels.garage.enums.EngineLayout;
+import com.dreamwheels.dreamwheels.garage.enums.EnginePosition;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.*;
 
 @Setter
@@ -12,7 +18,13 @@ import lombok.*;
 public class Vehicle extends Garage {
     private String vehicleMake;
     private String vehicleModel;
-    private String vehicleFuelType;
-    private String vehicleTransmission;
-    private int vehicleMileage;
+    @Enumerated(value = EnumType.STRING)
+    private DriveTrain driveTrain;
+    @Enumerated(value = EnumType.STRING)
+    private EngineLayout engineLayout;
+    @Enumerated(value = EnumType.STRING)
+    private EnginePosition enginePosition;
+    @Enumerated(value = EnumType.STRING)
+    private EngineAspiration vehicleEngineAspiration;
+
 }
