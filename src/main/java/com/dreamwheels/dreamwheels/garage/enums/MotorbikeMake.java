7fadd18d0080cd4ac127b5f057af038d9280836e
nyadero <briannyadero443@gmail.com>
2024-07-19 19:20:48 +0300

    motorbike make enum

---
+++ src/main/java/com/dreamwheels/dreamwheels/garage/enums/MotorbikeMake.java
@@ -0,0 +1,12 @@
+package com.dreamwheels.dreamwheels.garage.enums;
+
+public enum MotorbikeMake {
+    Honda,
+    Yamaha,
+    Ducati,
+    Kawasaki,
+    Bmw,
+    Suzuki,
+    Triumph,
+    KTM
+}
