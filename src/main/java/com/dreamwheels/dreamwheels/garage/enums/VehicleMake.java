2848b3b6286898ea730359e6748d933e5c0b6c96
nyadero <briannyadero443@gmail.com>
2024-07-22 09:42:11 +0300

    add dislay name for vehicle make

--- src/main/java/com/dreamwheels/dreamwheels/garage/enums/VehicleMake.java
+++ src/main/java/com/dreamwheels/dreamwheels/garage/enums/VehicleMake.java
@@ -1,37 +1,53 @@
 package com.dreamwheels.dreamwheels.garage.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum VehicleMake {
-    Abarth,
-    Alfa_Romeo,
-    Alpine,
-    Aston_Martin,
-    Audi,
-    Bentley,
-    Bmw,
-    Buggati,
-    Cadillac,
-    Chevrolet,
-    Dodge,
-    Ferrari,
-    Ford,
-    Honda,
-    Hyundai,
-    Jaguar,
-    Lamborghini,
-    LandRover,
-    Lotus,
-    Maserati,
-    Mazda,
-    Mclaren,
-    Mercedes,
-    Mitsubishi,
-    Nissan,
-    Peugeot,
-    Porsche,
-    Subaru,
-    Suzuki,
-    Toyota,
-    Volkswagen,
-    Volvo,
-    Other
+    Abarth("Abarth"),
+    Alfa_Romeo("Alfa Romeo"),
+    Alpine("Alpine"),
+    Aston_Martin("Aston Martin"),
+    Audi("Audi"),
+    Bentley("Bentley"),
+    Bmw("Bmw"),
+    Bugatti("Bugatti"),
+    Cadillac("Cadillac"),
+    Chevrolet("Chevrolet"),
+    Dodge("Dodge"),
+    Ferrari("Ferrari"),
+    Ford("Ford"),
+    Honda("Honda"),
+    Hyundai("Hyundai"),
+    Jaguar("Jaguar"),
+    Lamborghini("Lamborghini"),
+    LandRover("Land Rover"),
+    Lotus("Lotus"),
+    Maserati("Maserati"),
+    Mazda("Mazda"),
+    Mclaren("Mclaren"),
+    Mercedes("Mercedes"),
+    Mitsubishi("Mitsubishi"),
+    Nissan("Nissan"),
+    Peugeot("Peugeot"),
+    Porsche("Porsche"),
+    Subaru("Subaru"),
+    Suzuki("Suzuki"),
+    Toyota("Toyota"),
+    Volkswagen("Volkswagen"),
+    Volvo("Volvo"),
+    Other("Other");
+
+    private String displayName;
+
+    public static VehicleMake fromString(String value){
+        for (VehicleMake vehicleMake: VehicleMake.values()){
+            if (vehicleMake.getDisplayName().equalsIgnoreCase(value)){
+                return vehicleMake;
+            }
+        }
+        throw new IllegalArgumentException("Unknown vehicle make  " + value);
+    }
 }
