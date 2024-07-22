87ff34f94766df1a289d2c26182c7ef02ff6789c
nyadero <briannyadero443@gmail.com>
2024-07-22 09:33:33 +0300

    add motorbike category column

--- src/main/java/com/dreamwheels/dreamwheels/garage/entity/Motorbike.java
+++ src/main/java/com/dreamwheels/dreamwheels/garage/entity/Motorbike.java
@@ -1,6 +1,9 @@
 package com.dreamwheels.dreamwheels.garage.entity;
 
 import com.dreamwheels.dreamwheels.garage.enums.EngineAspiration;
+import com.dreamwheels.dreamwheels.garage.enums.MotorbikeCategory;
+import com.dreamwheels.dreamwheels.garage.enums.MotorbikeMake;
+import com.dreamwheels.dreamwheels.garage.enums.MotorbikeModel;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -13,8 +16,12 @@ import lombok.*;
 @Builder
 @Entity(name = "motorbike_garages")
 public class Motorbike extends Garage {
-    private String motorbikeMake;
-    private String motorbikeModel;
     @Enumerated(EnumType.STRING)
-    private EngineAspiration motorbikeEngineAspiration;
+    private MotorbikeMake motorbikeMake;
+
+    @Enumerated(EnumType.STRING)
+    private MotorbikeModel motorbikeModel;
+
+    @Enumerated(EnumType.STRING)
+    private MotorbikeCategory motorbikeCategory;
 }
