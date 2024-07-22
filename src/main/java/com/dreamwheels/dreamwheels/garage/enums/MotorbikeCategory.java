b1845df86efe84866734fe25337a5722d9a840af
nyadero <briannyadero443@gmail.com>
2024-07-22 08:14:23 +0300

    motorbike category enum

---
+++ src/main/java/com/dreamwheels/dreamwheels/garage/enums/MotorbikeCategory.java
@@ -0,0 +1,5 @@
+package com.dreamwheels.dreamwheels.garage.enums;
+
+public enum MotorbikeCategory {
+    Cruiser,Sports,Scooters,Adventure, Motorcross, Choppers, Enduro,Trial,CafeRacer, Touring,Quad,Trike;
+}
