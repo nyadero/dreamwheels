94773fcc5c9bc18aefe6c4b9e99384072052f003
nyadero <briannyadero443@gmail.com>
2024-07-24 15:30:16 +0300

    use the generic class to hold data fetched from database

--- src/main/java/com/dreamwheels/dreamwheels/configuration/responses/GarageApiResponse.java
+++ src/main/java/com/dreamwheels/dreamwheels/configuration/responses/GarageApiResponse.java
@@ -5,14 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class GarageApiResponse {
-    private Object data;
+public class GarageApiResponse<T> {
+    private Data<T> data;
     private String message;
     private ResponseType status;
 }
