19f8b503468c21a7be9de1b7ed761081c330a3a8
nyadero <briannyadero443@gmail.com>
2024-08-22 10:09:03 +0300

    entity not found exception

--- src/main/java/com/dreamwheels/dreamwheels/configuration/exceptions/EntityNotFoundException.java
+++ src/main/java/com/dreamwheels/dreamwheels/configuration/exceptions/EntityNotFoundException.java
@@ -1,6 +1,10 @@
 package com.dreamwheels.dreamwheels.configuration.exceptions;
 
+import com.dreamwheels.dreamwheels.configuration.responses.GarageApiResponse;
+import com.dreamwheels.dreamwheels.configuration.responses.ResponseType;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 public class EntityNotFoundException extends RuntimeException{
     public EntityNotFoundException(String message) {
