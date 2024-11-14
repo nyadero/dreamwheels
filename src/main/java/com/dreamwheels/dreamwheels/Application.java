b3682bf0b446fb79a892d10037374e44fc87d620
nyadero <briannyadero443@gmail.com>
2024-11-14 16:05:05 +0300

    load env

--- src/main/java/com/dreamwheels/dreamwheels/Application.java
+++ src/main/java/com/dreamwheels/dreamwheels/Application.java
@@ -1,6 +1,5 @@
 package com.dreamwheels.dreamwheels;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,27 +7,15 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.Objects;
-
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 @EnableCaching
-//@EnableAdminServer
 public class Application {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
-		// datasource
-		System.setProperty("SPRING_DATASOURCE_URL", Objects.requireNonNull(dotenv.get("SPRING_DATASOURCE_URL")));
-		System.setProperty("SPRING_DATASOURCE_USERNAME", Objects.requireNonNull(dotenv.get("SPRING_DATASOURCE_USERNAME")));
-		System.setProperty("SPRING_DATASOURCE_PASSWORD", Objects.requireNonNull(dotenv.get("SPRING_DATASOURCE_PASSWORD")));
-
-		// jwt
-		System.setProperty("JWT_SECRET_KEY", Objects.requireNonNull(dotenv.get("JWT_SECRET_KEY")));
-		System.setProperty("JWT_EXPIRATION", Objects.requireNonNull(dotenv.get("JWT_EXPIRATION")));
-		System.setProperty("JWT_REFRESH_TOKEN_EXPIRATION", Objects.requireNonNull(dotenv.get("JWT_REFRESH_TOKEN_EXPIRATION")));
-
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(Application.class, args);
 	}
 
