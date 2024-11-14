2a12ac44bedbc78e8adf865a8282d094acd79992
nyadero <briannyadero443@gmail.com>
2024-11-14 15:53:34 +0300

    findByEmail returns optional

--- src/main/java/com/dreamwheels/dreamwheels/auth/repository/AuthRepository.java
+++ src/main/java/com/dreamwheels/dreamwheels/auth/repository/AuthRepository.java
@@ -1,14 +1,19 @@
 package com.dreamwheels.dreamwheels.auth.repository;
 
 import com.dreamwheels.dreamwheels.users.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthRepository extends JpaRepository<User, String> {
-    User findByEmail(String email);
+   Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
 
     boolean existsByUserName(String userName);
+
 }
