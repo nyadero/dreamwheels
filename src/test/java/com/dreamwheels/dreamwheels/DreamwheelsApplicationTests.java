307b5e64bf871b58dc8eef4d4ccc02e91677b676
nyadero <briannyadero443@gmail.com>
2024-11-14 15:49:42 +0300

    application test class

--- src/test/java/com/dreamwheels/dreamwheels/DreamwheelsApplicationTests.java
+++ src/test/java/com/dreamwheels/dreamwheels/DreamwheelsApplicationTests.java
@@ -1,10 +1,19 @@
 package com.dreamwheels.dreamwheels;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class DreamwheelsApplicationTests {
+
+	@BeforeAll
+	static void setup() {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+	}
 
 	@Test
 	void contextLoads() {
