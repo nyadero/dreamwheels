54d2532e0ac2bf03fecc5887686efa0fcdeb6301
nyadero <briannyadero443@gmail.com>
2024-11-14 15:43:06 +0300

    findByToken should return optional

--- src/main/java/com/dreamwheels/dreamwheels/auth/repository/VerificationTokenRepository.java
+++ src/main/java/com/dreamwheels/dreamwheels/auth/repository/VerificationTokenRepository.java
@@ -4,7 +4,9 @@ import com.dreamwheels.dreamwheels.auth.entity.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, String> {
-    VerificationToken findByToken(String token);
+    Optional<VerificationToken> findByToken(String token);
 }
