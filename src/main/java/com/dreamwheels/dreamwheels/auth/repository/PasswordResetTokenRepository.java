757b1c1eb2c3705ded5d980c4b02d1fa54330768
nyadero <briannyadero443@gmail.com>
2024-11-14 15:43:12 +0300

    findByToken should return optional

--- src/main/java/com/dreamwheels/dreamwheels/auth/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/dreamwheels/dreamwheels/auth/repository/PasswordResetTokenRepository.java
@@ -4,7 +4,9 @@ import com.dreamwheels.dreamwheels.auth.entity.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, String> {
-    PasswordResetToken findByToken(String token);
+    Optional<PasswordResetToken> findByToken(String token);
 }
