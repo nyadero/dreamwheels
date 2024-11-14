b9ffd4289217d8010eb7ddc3719858fe88c8d417
nyadero <briannyadero443@gmail.com>
2024-11-14 15:52:01 +0300

    UseDetailsService to return optional User

--- src/main/java/com/dreamwheels/dreamwheels/configuration/ApplicationConfiguration.java
+++ src/main/java/com/dreamwheels/dreamwheels/configuration/ApplicationConfiguration.java
@@ -1,6 +1,7 @@
 package com.dreamwheels.dreamwheels.configuration;
 
 import com.dreamwheels.dreamwheels.auth.repository.AuthRepository;
+import com.dreamwheels.dreamwheels.users.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,7 +22,9 @@ public class ApplicationConfiguration {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return authRepository::findByEmail;
+        return username -> {
+            return authRepository.findByEmail(username).orElse(null);
+        };
     }
 
     @Bean
