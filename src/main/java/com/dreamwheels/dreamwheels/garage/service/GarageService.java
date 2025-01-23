65b02a768ea5c17e44e0535b8c3b79463e22fc69
nyadero <briannyadero443@@gmail.com>
2025-01-23 08:59:29 +0300

    moved to dtos/responses package

--- src/main/java/com/dreamwheels/dreamwheels/garage/service/GarageService.java
+++ src/main/java/com/dreamwheels/dreamwheels/garage/service/GarageService.java
@@ -1,29 +1,29 @@
 package com.dreamwheels.dreamwheels.garage.service;
 
 import com.dreamwheels.dreamwheels.configuration.responses.CustomPageResponse;
-import com.dreamwheels.dreamwheels.garage.dtos.GarageDto;
+import com.dreamwheels.dreamwheels.garage.dtos.GarageResponse;
 import com.dreamwheels.dreamwheels.garage.dtos.requests.MotorbikeGarageRequest;
 import com.dreamwheels.dreamwheels.garage.dtos.requests.VehicleGarageRequest;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface GarageService {
-    GarageDto addVehicleGarage(VehicleGarageRequest vehicleGarageDto, HttpServletRequest httpRequest);
+    GarageResponse addVehicleGarage(VehicleGarageRequest vehicleGarageDto, HttpServletRequest httpRequest);
 
-    GarageDto addMotorbikeGarage(MotorbikeGarageRequest motorbikeGarageDto, HttpServletRequest httpServletRequest);
+    GarageResponse addMotorbikeGarage(MotorbikeGarageRequest motorbikeGarageDto, HttpServletRequest httpServletRequest);
 
-    CustomPageResponse<GarageDto> allGarages(int pageNumber);
+    CustomPageResponse<GarageResponse> allGarages(int pageNumber);
 
-    GarageDto getGarageById(String id);
+    GarageResponse getGarageById(String id);
 
-    GarageDto updateVehicleGarage(VehicleGarageRequest vehicleGarageDto, String id, HttpServletRequest httpRequest);
+    GarageResponse updateVehicleGarage(VehicleGarageRequest vehicleGarageDto, String id, HttpServletRequest httpRequest);
 
-    GarageDto updateMotorbikeGarage(MotorbikeGarageRequest motorbikeGarageDto, String id, HttpServletRequest httpServletRequest);
+    GarageResponse updateMotorbikeGarage(MotorbikeGarageRequest motorbikeGarageDto, String id, HttpServletRequest httpServletRequest);
 
-    CustomPageResponse<GarageDto> garagesByCategory(String category, Integer pageNumber);
+    CustomPageResponse<GarageResponse> garagesByCategory(String category, Integer pageNumber);
 
-    CustomPageResponse<GarageDto> searchGarages(Integer pageNumber, String query, String vehicleMake, String vehicleModel, Integer mileage, Integer previousOwnersCount, Integer enginePower, Integer topSpeed, Integer acceleration, String transmissionType, String driveTrain, String enginePosition, String engineLayout, String engineAspiration, String bodyType);
+    CustomPageResponse<GarageResponse> searchGarages(Integer pageNumber, String query, String vehicleMake, String vehicleModel, Integer mileage, Integer previousOwnersCount, Integer enginePower, Integer topSpeed, Integer acceleration, String transmissionType, String driveTrain, String enginePosition, String engineLayout, String engineAspiration, String bodyType);
 
     void deleteGarage(String id);
 
-    CustomPageResponse<GarageDto> searchMotorbikeGarages(Integer pageNumber, String name, String motorbikeMake, String motorbikeModel, String motorbikeCategory1, Integer mileage, Integer previousOwnersCount, Integer enginePower, Integer topSpeed, Integer acceleration, String transmissionType, String engineAspiration, String engineLayout);
+    CustomPageResponse<GarageResponse> searchMotorbikeGarages(Integer pageNumber, String name, String motorbikeMake, String motorbikeModel, String motorbikeCategory1, Integer mileage, Integer previousOwnersCount, Integer enginePower, Integer topSpeed, Integer acceleration, String transmissionType, String engineAspiration, String engineLayout);
 }
