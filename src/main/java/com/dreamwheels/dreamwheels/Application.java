1daac7b4c392072dac18bc0da2baf0d4b93b37e8
nyadero <briannyadero443@@gmail.com>
2025-02-26 18:23:36 +0300

    load .env if RAILWAY_ENVIRONMENT is null

--- src/main/java/com/dreamwheels/dreamwheels/Application.java
+++ src/main/java/com/dreamwheels/dreamwheels/Application.java
@@ -14,8 +14,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
 
 	public static void main(String[] args) {
-//		Dotenv dotenv = Dotenv.load();
-//		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		if (System.getenv("RAILWAY_ENVIRONMENT") == null) {
+			Dotenv dotenv = Dotenv.load();
+			dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		}
 		SpringApplication.run(Application.class, args);
 	}
 
