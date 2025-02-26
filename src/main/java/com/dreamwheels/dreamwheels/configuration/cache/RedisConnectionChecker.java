7a49604807d56b02b2c5128622a81d0c623833ec
nyadero <briannyadero443@@gmail.com>
2025-02-26 12:50:15 +0300

    upload env

--- src/main/java/com/dreamwheels/dreamwheels/configuration/cache/RedisConnectionChecker.java
+++ src/main/java/com/dreamwheels/dreamwheels/configuration/cache/RedisConnectionChecker.java
@@ -3,7 +3,6 @@ package com.dreamwheels.dreamwheels.configuration.cache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +11,10 @@ public class RedisConnectionChecker {
 
     @Autowired
     private RedisConnectionFactory redisConnectionFactory;
+
+//    public RedisConnectionChecker(RedisConnectionFactory redisConnectionFactory) {
+//        this.redisConnectionFactory = redisConnectionFactory;
+//    }
 
     @Bean
     public CommandLineRunner checkRedisConnection() {
@@ -24,16 +27,16 @@ public class RedisConnectionChecker {
             }
         };
     }
-    
+
     private boolean checkConnection(){
         try{
             var connection = redisConnectionFactory.getConnection();
-            System.out.println(connection);
+            System.out.println(connection.ping());
             // Sends a PING command to test the connection
             connection.ping();
             return true;
         }catch (Exception e){
-            System.out.println(e);
+//            log.error("e: ", e);
             return false;
         }
     }
--- src/main/java/com/dreamwheels/dreamwheels/garage/adapters/GarageMapper.java
+++ src/main/java/com/dreamwheels/dreamwheels/garage/adapters/GarageMapper.java
@@ -1,10 +1,30 @@
 package com.dreamwheels.dreamwheels.garage.adapters;
 
 import com.dreamwheels.dreamwheels.garage.dtos.responses.GarageResponse;
+import com.dreamwheels.dreamwheels.garage.dtos.responses.MotorbikeResponse;
+import com.dreamwheels.dreamwheels.garage.dtos.responses.VehicleResponse;
 import com.dreamwheels.dreamwheels.garage.entity.Garage;
+import com.dreamwheels.dreamwheels.garage.entity.Motorbike;
+import com.dreamwheels.dreamwheels.garage.entity.Vehicle;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface GarageMapper {
-    GarageResponse toDto(Garage garage);
+    GarageMapper INSTANCE = Mappers.getMapper(GarageMapper.class);
+
+    GarageResponse toGarageDTO(Garage garage);
+
+    MotorbikeResponse toMotorbikeDTO(Motorbike motorbike);
+
+    VehicleResponse toVehicleDTO(Vehicle vehicle);
+
+//    List<GarageDTO> toGarageDTOList(List<Garage> garages);
+
+//    Garage toGarageEntity(GarageDTO garageDTO);
+//
+//    Motorbike toMotorbikeEntity(MotorbikeDTO motorbikeDTO);
+//
+//    Vehicle toVehicleEntity(VehicleDTO vehicleDTO);
+
 }
