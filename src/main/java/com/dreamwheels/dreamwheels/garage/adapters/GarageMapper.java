783023d40d51a47f9ec2c07338f0eaaa432acc51
nyadero <briannyadero443@@gmail.com>
2025-02-27 21:12:20 +0300

    remove ambigous toGarageResponse

--- src/main/java/com/dreamwheels/dreamwheels/garage/adapters/GarageMapper.java
+++ src/main/java/com/dreamwheels/dreamwheels/garage/adapters/GarageMapper.java
@@ -12,23 +12,24 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface GarageMapper {
-
-    // Automatically detect subclass and return the correct response type
-    default GarageResponse toGarageDto(Garage garage){
-        if (garage instanceof Motorbike){
-            return toMotorbikeResponse((Motorbike) garage);
-        } else if (garage instanceof Vehicle) {
-            return toVehicleResponse((Vehicle) garage);
-        }
-        return toGarageResponse(garage);
-    }
-
-    GarageResponse toGarageResponse(Garage garage);
+//    GarageResponse toGarageResponse(Garage garage);
 
     @InheritConfiguration(name = "toGarageResponse")
     MotorbikeResponse toMotorbikeResponse(Motorbike motorbike);
 
     @InheritConfiguration(name = "toGarageResponse")
     VehicleResponse toVehicleResponse(Vehicle vehicle);
+
+    // Automatically detect subclass and return the correct response type
+    default GarageResponse toGarageResponse(Garage garage){
+        if (garage instanceof Motorbike){
+            return toMotorbikeResponse((Motorbike) garage);
+        } else if (garage instanceof Vehicle) {
+            return toVehicleResponse((Vehicle) garage);
+        }
+        return null;
+//        return toGarageResponse(garage);
+    }
+
 
 }
